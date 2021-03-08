bd5287ef1eabb783b5c8fab349689279b29e9204
FedericoBeeche <fbeeche@gmail.com>
2021-03-08 18:25:40 +0000

    validation dejo de servir

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-window.onload = function () {
+window.onload = function validation() {
+
+var cardNum = document.getElementById('tarjetaCredito');
+var cardCVC = document.getElementById('CVCTarjeta');
+var amountUSD = document.getElementById('montoDolares');
+var name = document.getElementById('firstName');
+var nameLast = document.getElementById('lastName');
+var ciudad = document.getElementById('ciudadInput');
+var postalCode = document.getElementById('codigoPostal');
+var error = document.getElementsByClassName('errorAlert');
+
+var formValidate = document.getElementById('formulario');
+formValidate.addEventListener('submit', function (event){
+    event.preventDefault();
+    if(cardNum.value.length < 13 || cardNum.value.length > 19 || cardNum.value.length === ''){
+        error.innerHTML = "Please enter a valid CC Number"
+
+    if(cardCVC.value.length !== 3 || cardCVC.value.length > 3 || cardCVC.value.length === ''){
+        error.innterHTML = "Please enter a valid CVC Number"
+    }  
+    
+    } else{
+        alert("Form sent successfully!")
+    }
+})
 
 }
