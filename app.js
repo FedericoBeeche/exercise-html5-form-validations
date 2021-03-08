228683d655c29ae9fe621fa66c79827efc9eda45
FedericoBeeche <fbeeche@gmail.com>
2021-03-08 20:32:10 +0000

    avances

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 window.onload = function validation() {
 
-var cardNum = document.getElementById('tarjetaCredito');
-var cardCVC = document.getElementById('CVCTarjeta');
-var amountUSD = document.getElementById('montoDolares');
-var name = document.getElementById('firstName');
-var nameLast = document.getElementById('lastName');
-var ciudad = document.getElementById('ciudadInput');
-var postalCode = document.getElementById('codigoPostal');
-var error = document.getElementsByClassName('errorAlert');
+    var cardNum = document.getElementById('tarjetaCredito');
+    var cardCVC = document.getElementById('CVCTarjeta');
+    var amountUSD = document.getElementById('montoDolares');
+    var name = document.getElementById('firstName');
+    var nameLast = document.getElementById('lastName');
+    var ciudad = document.getElementById('ciudadInput');
+    var postalCode = document.getElementById('codigoPostal');
+    var errorCC = document.getElementById('errorCC');
+    var errorCVC = document.getElementById('errorCVC');
 
-var formValidate = document.getElementById('formulario');
-formValidate.addEventListener('submit', function (event){
-    event.preventDefault();
-    if(cardNum.value.length < 13 || cardNum.value.length > 19 || cardNum.value.length === ''){
-        error.innerHTML = "Please enter a valid CC Number"
+    var formValidate = document.getElementById('formulario');
+    formValidate.addEventListener('submit', function (event) {
+        event.preventDefault();
+        if (cardNum.value.length < 13 || cardNum.value.length > 19 || cardNum.value.length === '') {
+            errorCC.innerHTML = "Please enter a valid CC Number";
+        }
+        else if (cardCVC.value.length !== 3 || cardCVC.value.length > 3 || cardCVC.value.length === '') {
+            errorCVC.innterHTML = "Please enter a valid CVC Number";
+        } 
+        
+        else {
+            alert("Form sent successfully!")
+        }
 
-    if(cardCVC.value.length !== 3 || cardCVC.value.length > 3 || cardCVC.value.length === ''){
-        error.innterHTML = "Please enter a valid CVC Number"
-    }  
-    
-    } else{
-        alert("Form sent successfully!")
-    }
-})
+    })
 
 }
